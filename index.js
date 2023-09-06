62c12032356916b88bdb2ae5ea93f88bac9455bb
navya-gangidi <navyareddy1999@gmail.com>
2023-09-06 13:41:38 -0400

    Google OAuth Added

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
-const express = require('express')
+const express = require('express');
+require('./services/passport');
 
 const app = express();
-
-app.get('/', (req, res) =>{
-    res.send({hi: 'there'});
-});
-
-//const PORT = process.env.PORT - used in production environment
-//To run in a dev environment - sometimes it might not be set by Heroku therefore we'll use 5000 instead.
+require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT);
